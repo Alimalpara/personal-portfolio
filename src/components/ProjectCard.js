83765d77f4ce54ecc66ff2b568d6e7d50ddc102e
Alimalpara <work.alimalpara@gmail.com>
2024-04-09 15:55:53 -0400

    added favicon

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,42 @@
-import { Col } from "react-bootstrap";
+// import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+// export const ProjectCard = ({ title, description, imgUrl }) => {
+//   return (
+//     <Col size={12} sm={6} md={4}>
+//       <div className="proj-imgbx">
+//         <img src={imgUrl} />
+//         <div className="proj-txtx">
+//           <h4>{title}</h4>
+//           <span>{description}</span>
+//         </div>
+//       </div>
+//     </Col>
+//   )
+// }
+import React from "react";
+import { Card, Button, Carousel } from "react-bootstrap";
+
+export const ProjectCard = ({ title, description, images, link }) => {
   return (
-    <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={imgUrl} />
-        <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
-        </div>
-      </div>
-    </Col>
-  )
-}
+    <Card className="project-card" style={{ marginBottom: "20px" }}>
+      <Carousel
+        interval={null}
+        indicators={false}
+        style={{ marginBottom: "10px" }}
+      >
+        {images.map((image, index) => (
+          <Carousel.Item key={index}>
+            <img className="d-block w-100" src={image} alt={title} />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+        <Button variant="primary" href={link} target="_blank">
+          View Project
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,42 +4,119 @@ import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+
+// export const Projects = () => {
+
+//   const projects = [
+//     {
+//       title: "Android Application",
+//       description: "Ace It",
+//       imgUrl: projImg1,
+//     },
+//     {
+//       title: "Business Startup",
+//       description: "Design & Development",
+//       imgUrl: projImg2,
+//     },
+//     {
+//       title: "Business Startup",
+//       description: "Design & Development",
+//       imgUrl: projImg3,
+//     },
+//     {
+//       title: "Business Startup",
+//       description: "Design & Development",
+//       imgUrl: projImg1,
+//     },
+//     {
+//       title: "Business Startup",
+//       description: "Design & Development",
+//       imgUrl: projImg2,
+//     },
+//     {
+//       title: "Business Startup",
+//       description: "Design & Development",
+//       imgUrl: projImg3,
+//     },
+//   ];
+
+//   return (
+//     <section className="project" id="projects">
+//       <Container>
+//         <Row>
+//           <Col size={12}>
+//             <TrackVisibility>
+//               {({ isVisible }) =>
+//               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+//                 <h2>Projects</h2>
+//                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+//                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
+//                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+//                     <Nav.Item>
+//                       <Nav.Link eventKey="first">Tab 1</Nav.Link>
+//                     </Nav.Item>
+//                     <Nav.Item>
+//                       <Nav.Link eventKey="second">Tab 2</Nav.Link>
+//                     </Nav.Item>
+//                     <Nav.Item>
+//                       <Nav.Link eventKey="third">Tab 3</Nav.Link>
+//                     </Nav.Item>
+//                   </Nav>
+//                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+//                     <Tab.Pane eventKey="first">
+//                       <Row>
+//                         {
+//                           projects.map((project, index) => {
+//                             return (
+//                               <ProjectCard
+//                                 key={index}
+//                                 {...project}
+//                                 />
+//                             )
+//                           })
+//                         }
+//                       </Row>
+//                     </Tab.Pane>
+//                     <Tab.Pane eventKey="section">
+//                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+//                     </Tab.Pane>
+//                     <Tab.Pane eventKey="third">
+//                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+//                     </Tab.Pane>
+//                   </Tab.Content>
+//                 </Tab.Container>
+//               </div>}
+//             </TrackVisibility>
+//           </Col>
+//         </Row>
+//       </Container>
+//       <img className="background-image-right" src={colorSharp2}></img>
+//     </section>
+//   )
+// }
+
+// import React from "react";
+// import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+// import { ProjectCard } from "./ProjectCard";
+// import colorSharp2 from "../assets/img/color-sharp2.png";
 
 export const Projects = () => {
-
   const projects = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
+      title: "Android Application",
+      description: "Ace It",
+      images: [projImg1, projImg2, projImg3],
+      link: "https://example.com/android-project",
     },
     {
       title: "Business Startup",
       description: "Design & Development",
-      imgUrl: projImg2,
+      images: [projImg1, projImg2],
+      link: "https://example.com/business-project",
     },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
+    // Add more projects with respective images and links
   ];
 
   return (
@@ -47,52 +124,36 @@ export const Projects = () => {
       <Container>
         <Row>
           <Col size={12}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
-            </TrackVisibility>
+            <div>
+              <h2>Projects</h2>
+              <p>
+                Explore some of the projects I've worked on. From web
+                development to mobile applications, each project showcases my
+                skills and expertise in software development.
+              </p>
+              <p>
+                Whether it's building sleek user interfaces or architecting
+                robust backend systems, I strive to deliver high-quality
+                solutions that meet the needs of clients and users alike. Below
+                are a few selected projects that demonstrate my capabilities
+                across different technologies and domains.
+              </p>
+            </div>
+            <Row>
+              {projects.map((project, index) => (
+                <Col key={index} sm={12} md={6} lg={4}>
+                  <ProjectCard {...project} />
+                </Col>
+              ))}
+            </Row>
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
+      <img
+        className="background-image-right"
+        src={colorSharp2}
+        alt="Background"
+      />
     </section>
-  )
-}
+  );
+};
